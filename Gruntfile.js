f6dc7fcd9d6b0d6192616ec40b705a4b816d1eb1
Vivek Aithal K R <nuwanda@Viveks-MacBook-Air.local>
2017-10-19 13:47:51 +0530

    gruntifying

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		clean: ['dist'],
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					src: ['index.html','jsmain.js','cssmain.css', 'next.html','data.js','arnab.jpg','fbog.jpg','jquery.waypoints.min.js','refmig.jpg'],
+					dest: 'dist/'
+				}]
+			}
+		},
+				
+		'gh-pages': {
+			options: {
+				base: 'dist'
+			},
+			src: ['**']
+		}
+	});
+
+    grunt.registerTask('build', ['clean', 'copy:dist']);
+
+	require('load-grunt-tasks')(grunt);
+};
